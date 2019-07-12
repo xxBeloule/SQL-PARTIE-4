24fd45bd84986aff550bb6c3d9d2a2a4db484909
xxBeloule <trezaaqq@hotmail.fr>
2019-07-12 14:17:05 +0200

    Premier commit

---
+++ exo1/exo1.sql
@@ -0,0 +1,10 @@
+INSERT INTO languages (languages, version)
+VALUES
+('JavaScript', 'version 5'),
+('PHP', 'version 5.2'),
+('PHP', 'version 5.1'),
+('HTML', 'version 5.1'),
+('JavaScript', 'version 6'),
+('JavaScript', 'version 7'),
+('JavaScript', 'version 8'),
+('PHP', 'version 7');
---
+++ exo2/exo2.sql
@@ -0,0 +1,6 @@
+INSERT INTO frameworks (framework,version)
+VALUES
+('Symfony', 'version 2.8'),
+('Symfony', 'version 3'),
+('Jquery', 'version 1.6'),
+('Jquery', 'version 2.10');
